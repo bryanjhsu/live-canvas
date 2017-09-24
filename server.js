0777f6ce17d01586d96d64f1e68ef93fc0d54976
Youn ho Lee <iunolee@172-16-246-254.DYNAPOOL.NYU.EDU>
2017-09-24 17:19:40 -0400

    first

---
+++ server.js
@@ -0,0 +1,45 @@
+// HTTP Portion
+var http = require('http');
+var fs = require('fs'); // Using the filesystem module
+var httpServer = http.createServer(requestHandler);
+var url = require('url');
+httpServer.listen(8080);
+
+function requestHandler(req, res) {
+
+  var parsedUrl = url.parse(req.url);
+  console.log("The Request is: " + parsedUrl.pathname);
+
+  fs.readFile(__dirname + parsedUrl.pathname,
+    // Callback function for reading
+    function(err, data) {
+      // if there is an error
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading ' + parsedUrl.pathname);
+      }
+      // Otherwise, send the data, the contents of the file
+      res.writeHead(200);
+      res.end(data);
+    }
+  );
+
+}
+
+
+// WebSocket Portion
+var io = require('socket.io').listen(httpServer);
+
+io.sockets.on('connection',
+  // We are given a websocket object in our function
+  function(socket) {
+    console.log("We have a new client: " + socket.id);
+    socket.on('drawing', function(data) {
+      // Data comes in as whatever was sent, including objects
+      console.log("Received: 'drawing' " + data);
+
+      // Send it to all of the clients
+      io.emit('drawing', data);
+    });
+  }
+);
